a9edab82cde703da2ba4353e2ecf5df0fafce552
@rajat1106 <rajatharne@gmail.com>
2023-05-06 19:56:19 +0530

    footer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
+import Footer from '../components/Footer'
+
+
+
 
 function Home() {
   return (
     <>
         <Navbar/>
         <Main/>
+        <Footer/>
     </>
   )
 }
